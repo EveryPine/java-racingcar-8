e296abf3c8ff50156d9fcd1df2987ff2f14ef2a9
EveryPine <kenton.lim@gmail.com>
2025-10-25 00:50:39 +0900

    style: 경주 자동차 이름 입력 안내 문구의 괄호 누락 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -5,7 +5,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
 
     public static String getCarName() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = readLine();
 
         return input;
